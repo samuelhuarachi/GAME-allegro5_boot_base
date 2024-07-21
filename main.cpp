9a7e47c6fdaac42d11b15486769651d893f8a1d3
Samuel Gomes <samuel.huarachi@gmail.com>
2024-07-21 13:48:11 -0300

    allegro_bootstrap

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <typeinfo>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <math.h>
+#include <array>
+#include <iterator>
+#include <cmath>
+#include <tuple>
+using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::vector;
+#define KEY_SEEN     1
+#define KEY_RELEASED 2
+
+bool is_pressing_the_key(int key) {
+    if (key) {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    ALLEGRO_DISPLAY* display;
+
+    if(!al_init())
+        al_show_native_message_box(NULL,NULL,NULL,"Allegro couldnt initialize",NULL,NULL);
+
+    if(!al_install_keyboard())
+    {
+        printf("couldn't initialize keyboard\n");
+    }
+    /*if(!al_install_mouse())
+    {
+        printf("couldn't initialize mouse\n");
+    }*/
+    al_init_primitives_addon();
+    al_install_audio();
+    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+    al_init_acodec_addon();
+
+    ALLEGRO_FONT* font;
+    font = al_create_builtin_font();
+
+    //al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+    display = al_create_display(800,600);
+    if(!display)
+        al_show_native_message_box(NULL,NULL,NULL,"Couldnt create Screen",NULL,NULL);
+
+
+    ALLEGRO_EVENT_QUEUE* queue;
+
+    ALLEGRO_TIMER* timer;
+    timer = al_create_timer(1.0 / 60.0);
+    queue = al_create_event_queue();
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    //al_register_event_source(queue, al_get_mouse_event_source());
+
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+    unsigned char key[ALLEGRO_KEY_MAX];
+
+    if(!al_init_image_addon())
+        al_show_native_message_box(NULL,NULL,NULL,"Allegro image addon couldnt initialize",NULL,NULL);
+
+
+    bool abc = false;
+    bool mb1 = false;
+
+    int pos_x = 1;
+    int pos_y = 1;
+
+    bool exit_game = false;
+    while(!exit_game) {
+        al_wait_for_event(queue, &event);
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER: // Holding key
+                abc = is_pressing_the_key(key[ALLEGRO_KEY_S]);
+                if (abc) {
+                        printf("%d\n", abc);
+                }
+
+                for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
+                    key[i] &= KEY_SEEN;
+
+                break;
+            case ALLEGRO_EVENT_KEY_DOWN: // Key down
+                key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
+                break;
+            case ALLEGRO_EVENT_KEY_UP: // Key up
+                if(key[ALLEGRO_KEY_A]) {
+                }
+                key[event.keyboard.keycode] &= KEY_RELEASED;
+                break;
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                exit_game = true;
+                break;
+            /*case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+                pos_x = event.mouse.x;
+                pos_y = event.mouse.y;
+                break;*/
+
+        }
+
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 10, 20, 0, "X: %f", 1.1);
+
+        al_flip_display();
+    }
+
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
